23adfdcd0d47e6761407898b4ae44e8a78897fde
olamic <olawuyim338@gmail.com>
2022-03-10 07:25:14 -0800

    do

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 
 
 /**
  *
- *  * main - Prints written quote
+ *  * main - Entry point
  *
  *   *
  *
- *    * Return: zero on success
+ *    * Return: Always 0 (Success)
  *
- *     *
- *
- *      */
+ *     */
+
+
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	 puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	  return (0);
 
 }
