ad95ddc8b79496547825b62abfe43893a17cc1c9
olamic <olawuyim338@gmail.com>
2022-03-10 08:07:57 -0800

    new

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  *main -  print quote using printf
+ *
+ *   *Description: very simple snippet
+ *
+ *    *Return: 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+
+			return (0);
+
+}
