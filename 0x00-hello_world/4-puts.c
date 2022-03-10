5da1c9fd94c2252c6f7e9790ea6150c33338f0ac
olamic <olawuyim338@gmail.com>
2022-03-10 07:18:12 -0800

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Prints written quote
+ *
+ *   *
+ *
+ *    * Return: zero on success
+ *
+ *     *
+ *
+ *      */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
