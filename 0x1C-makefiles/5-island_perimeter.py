f9752e74289caac3f615a6ae560516f57ced5c2f
Michael olawuyi <52048776+olamic@users.noreply.github.com>
2022-08-16 22:44:59 -0700

    alx mapami na

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    """
+    row = len(grid)
+    if row == 0:
+        return 0
+    column = len(grid[0])
+
+    perimeter = 0
+
+    for r in range(row):
+        for c in range(column):
+            if grid[r][c] == 1:
+                perimeter += 4
+                if c > 0 and grid[r][c - 1] == 1:
+                    perimeter -= 2
+                if r > 0 and grid[r - 1][c] == 1:
+                    perimeter -= 2
+    return perimeter
